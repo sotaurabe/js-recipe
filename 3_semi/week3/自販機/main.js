c6f8e80b8baca00428af5628c74ff2ddfc0b642e
sotaurabe <tacchoso@icloud.com>
2021-05-02 19:57:20 +0900

    カレーの紹介

--- 3_semi/week3/自販機/main.js
+++ 3_semi/week3/自販機/main.js
@@ -1,10 +1,12 @@
 const itemsContainer = document.getElementById("items-container")
 const teaButton = document.getElementById("tea-button")
 const cokeButton = document.getElementById("coke-button")
+const beerButton = document.getElementById("beer-button")
 const myMoneyDisplay = document.getElementById("my-money")
 const amountMoneyDisplay = document.getElementById("amount-money")
 const addMyMoneyButton = document.getElementById("add-my-money")
 const addAmountMoneyButton = document.getElementById("add-amount-money")
+const changeBUtton = document.getElementById("oturi-button")
 
 let myMoney = 0
 let amountMoney = 0
@@ -42,21 +44,49 @@ cokeButton.onclick = function() {
   }
 }
 
+beerButton.onclick = function() {
+  if (amountMoney >= 150) {
+    amountMoney = amountMoney - 150
+    amountMoneyDisplay.textContent = amountMoney
+    const image = createImage("beer")
+    itemsContainer.append(image)
+  }
+}
+
+changeBUtton.onclick = function() {
+  myMoney = myMoney + amountMoney
+  amountMoney = 0
+  amountMoneyDisplay.textContent = amountMoney
+  myMoneyDisplay.textContent = myMoney
+}
+
 const createImage = (drink) => {
   let drinkImgSrc = ""
-  switch (drink) {
+
+  if (drink === "tea") {
+    drinkImgSrc =
+      "https://www.itoen.jp/files/products/japanese_tea/210315%20ikiikiPKG.jpg"
+  } else if (drink === "coke") {
+    drinkImgSrc =
+      "https://www.cocacola.jp/images/product/cola_bottle_img_2020.png"
+  } else if (drink === "beer") {
+    drinkImgSrc =
+      "https://image.itmedia.co.jp/news/articles/2104/22/l_ts0153_zenkai01.jpg"
+    ;("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.biccamera.com%2Fbc%2Fi%2Ftopics%2Fosusume_beer%2Findex.jsp&psig=AOvVaw2ENfbLvxV-65qcMGUGhuJD&ust=1620035322838000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjw74LcqvACFQAAAAAdAAAAABAV")
+  }
+  /* switch (drink) {
     case "tea":
       drinkImgSrc =
-        "https://www.itoen.jp/files/products/japanese_tea/210315%20ikiikiPKG.jpg"
-      break
+        "https://www.itoen.jp/files/products/japanese_tea/210315%20ikiikiPKG.jpg";
+      break;
     case "coke":
       drinkImgSrc =
-        "https://www.cocacola.jp/images/product/cola_bottle_img_2020.png"
-      break
+        "https://www.cocacola.jp/images/product/cola_bottle_img_2020.png";
+      break;
     default:
       drinkImgSrc =
-        "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-  }
+        "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"; */
+  //}
   const figure = document.createElement("figure")
   figure.classList.add("vending-machine__items-container__figure")
   const img = document.createElement("img")
